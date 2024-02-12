a0be1ac4c7b50c48d7db4615d6378dc8756cc666
defabdullah <ahmet.susuz@boun.edu.tr>
2024-02-13 01:41:07 +0300

    fix import

--- snowification/Fid/fid_score.py
+++ snowification/Fid/fid_score.py
@@ -49,8 +49,7 @@ except ImportError:
     def tqdm(x):
         return x
 
-from Fid import InceptionV3
-
+from Fid.inception import InceptionV3
 # parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
 # parser.add_argument('--batch-size', type=int, default=50,
 #                     help='Batch size to use')
