b0130c1966ec7aed557f2b77237d03e97554b817
Harun Reşid Ergen <ergenharunresid@hotmail.com>
2024-02-26 21:31:55 +0300

    Add argument to calculate_accuracy.py

--- snowification/helpers/calculate_accuracy.py
+++ snowification/helpers/calculate_accuracy.py
@@ -4,6 +4,7 @@ from resnet_classifier import ResNetClassifier
 parser = argparse.ArgumentParser()
 parser.add_argument('--folder_path', type=str)
 parser.add_argument('--model_path', default='./resnet18.pt', type=str)
+parser.add_argument('--dataset_folder', default="", type=str)
 
 args = parser.parse_args()
 
@@ -11,4 +12,4 @@ args = parser.parse_args()
 classifier = ResNetClassifier(model_path=args.model_path)
 
 print(f"Calculating accuracy of the classifier on the images in the folder: '{args.folder_path}'", )
-print(f"Accuracy: {classifier.calculate_accuracy(folder_path=args.folder_path)}")
+print(f"Accuracy: {classifier.calculate_accuracy(folder_path=args.folder_path, test_dataset_path=args.dataset_folder)}")
