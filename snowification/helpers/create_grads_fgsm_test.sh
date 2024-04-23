3bbe1a4dfe3665fcedb2d029579ac3ecd6a19bf2
defabdullah <ahmet.susuz@boun.edu.tr>
2024-04-23 04:06:29 +0300

    name changing

--- snowification/helpers/create_grads_fgsm_test.sh
+++ snowification/helpers/create_grads_fgsm_test.sh
@@ -5,4 +5,4 @@
 #SBATCH --mem-per-gpu=40G
 
 source /opt/python3/venv/base/bin/activate
-python3 '/opt/python3/venv/base/helpers/create_grads_fgsm.py' --root '/users/ahmet.susuz/cifar_10_test_grads' --model_path '/users/ahmet.susuz/resnet18.pt' --data_type 'test' --batch_size 32
+python3 '/opt/python3/venv/base/helpers/create_grads_fgsm.py' --root '/users/harun.ergen/cifar_10_test_grads' --model_path '/users/harun.ergen/resnet18.pt' --data_type 'test' --batch_size 32
