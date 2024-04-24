b3d8601874e42ef95bb162c190240b68cbd88cd0
defabdullah <ahmet.susuz@boun.edu.tr>
2024-04-24 08:20:53 +0300

    .

--- snowification/testing_script.sh
+++ snowification/testing_script.sh
@@ -5,4 +5,4 @@
 #SBATCH --mem-per-gpu=40G
 
 source /opt/python3/venv/base/bin/activate
-python3 '/opt/python3/venv/base/test.py' --model 'UnetResNet' --dataset 'cifar10' --time_steps 50 --test_type 'test_data' --dataset_folder '/users/harun.ergen/cifar_10_test_dataset/clean' --save_folder_test '/users/harun.ergen/test_results/2_26_130k_clean' --save_folder_train '/users/harun.ergen/train_results/130k_epoch_50ts' --grad_folder '/users/harun.ergen/cifar_10_train_grads' --forward_process_type 'FGSM'
+python3 '/opt/python3/venv/base/test.py' --model 'UnetResNet' --dataset 'cifar10' --time_steps 50 --test_type 'test_data' --dataset_folder '/users/harun.ergen/cifar_10_test_dataset/clean' --save_folder_test '/users/harun.ergen/test_results/2_26_130k_clean' --save_folder_train '/users/harun.ergen/train_results/130k_epoch_50ts' --grad_folder '/users/harun.ergen/cifar_10_test_grads' --forward_process_type 'FGSM'
